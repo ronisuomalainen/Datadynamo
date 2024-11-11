70d9f4dec104414c0edd07393be2c78696702b3a
ronisuomalainen <rsuomala@edu.lapinamk.fi>
2024-11-11 13:15:50 +0200

    login

--- datadynamo-store/src/pages/Login.jsx
+++ datadynamo-store/src/pages/Login.jsx
@@ -7,14 +7,12 @@ const Login = () => {
       <h2>Kirjaudu sisään</h2>
       <form>
         <div className="form-group">
-          <label htmlFor="username">Käyttäjätunnus</label>
-          <input type="text" id="username" placeholder="Enter username" required />
+          <input type="text" id="username" placeholder="Käyttäjätunnus" required />
         </div>
         <div className="form-group">
-          <label htmlFor="password">Salasana</label>
-          <input type="password" id="password" placeholder="Enter password" required />
+          <input type="password" id="password" placeholder="Salasana" required />
         </div>
-        <button type="submit">Kirjaudu</button>
+        <button type="submit">Kirjaudu</button> 
       </form>
     </div>
   )
