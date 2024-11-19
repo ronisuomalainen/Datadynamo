2f979aa0cd0ca05fe65cc0ab38d56514dbffcc68
samuheinonen <samu.heinone@gmail.com>
2024-11-19 15:45:57 +0200

    Store page lisätty store pagelle ulkoasu pitää vielä hioa

--- datadynamo-store/src/pages/Store.jsx
+++ datadynamo-store/src/pages/Store.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import '../index.css'
 const Store = () => {
   return (
-    <div>Store</div>
+    <div class="content">
+    <div class="image-section">
+      <img src="matto.webp" alt="Tuotekuva"/>
+      <button>Lisää kuva</button>
+    </div>
+    <div class="details-section">
+      <p>Lorem ipsum hyvä hiirimatto omalla kuvalla</p>
+      <p>1,50€</p>
+      <button>Osta</button>
+    </div>
+  </div>
   )
 }
 
