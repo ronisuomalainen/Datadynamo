45b05bbc0b35b6704d0504bc49941d18e7722d37
ronisuomalainen <146922686+ronisuomalainen@users.noreply.github.com>
2024-11-20 12:27:51 +0200

    cleanup ja korjattu css

--- datadynamo-store/src/pages/Store.jsx
+++ datadynamo-store/src/pages/Store.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
 import '../index.css'
+
 const Store = () => {
   return (
-    <div class="content">
-    <div class="image-section">
+    <div className="store-content">
+    <div className="image-section">
       <img src="matto.webp" alt="Tuotekuva"/>
       <button>Lisää kuva</button>
     </div>
-    <div class="details-section">
+    <div className="details-section">
       <p>Lorem ipsum hyvä hiirimatto omalla kuvalla</p>
       <p>1,50€</p>
       <button>Osta</button>
