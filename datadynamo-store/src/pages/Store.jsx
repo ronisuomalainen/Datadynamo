125f62e2df9561c196e7730c0f02728f2b2c55f0
ronisuomalainen <146922686+ronisuomalainen@users.noreply.github.com>
2024-11-25 15:52:27 +0200

    säädetty storepagea

--- datadynamo-store/src/pages/Store.jsx
+++ datadynamo-store/src/pages/Store.jsx
@@ -1,4 +1,4 @@
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import '../index.css'
 
 const Store = () => {
@@ -10,7 +10,7 @@ const Store = () => {
 
   return (
     
-    <div className="store-content">
+  <div className="store-content">
     <div className="image-section">
       <img src="matto.webp" alt="Tuotekuva"/>
       <button className='add_Picture'>Lisää kuva</button>
@@ -18,7 +18,7 @@ const Store = () => {
     <form onSubmit={handlePurchase}>
     <div className="details-section">
       <p>Lorem ipsum hyvä hiirimatto omalla kuvalla</p>
-      <p>1,50€</p>
+      <p>40€</p>
       <button className='purchase_Btn'>Osta</button>
     </div>
     </form>
