15a1628ca4a8598d97e484d0fdcca15d222d6e63
Rabbawabba <135864443+Rabbawabba@users.noreply.github.com>
2024-12-08 16:50:33 +0200

    ci/cd ja paljon korjailua

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,21 +1,21 @@
-import '../index.css'
-import { Link } from 'react-router-dom'
+import '../index.css';
+import { Link } from 'react-router-dom';
 
 const Welcome = () => {
   return (
-    <div className='welcome-container'>
-        <h1>Tervetuloa Datadynamoon!</h1>
-        <p>Kirjaudu sisään tai rekisteröidy alla</p>
-        <div className='button-container'>
-            <Link to='/login' className="button">
-                Kirjaudu
-            </Link>
-            <Link to='/register' className='button'>
-                Rekisteröidy
-            </Link>
-        </div>
+    <div className="welcome-container">
+      <h1>Tervetuloa Datadynamoon....!</h1>
+      <p>Kirjaudu sisään tai rekisteröidy alla</p>
+      <div className="button-container">
+        <Link to="/login" className="button">
+          Kirjaudu
+        </Link>
+        <Link to="/register" className="button">
+          Rekisteröidy
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
