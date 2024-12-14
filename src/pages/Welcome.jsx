f1b20812f008d50ac0fde757a5635a1fc3833f61
ronisuomalainen <146922686+ronisuomalainen@users.noreply.github.com>
2024-12-14 18:16:00 +0200

    Deployn edit

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -5,7 +5,7 @@ const Welcome = () => {
   return (
     <div className="welcome-container">
       <h1>Tervetuloa Datadynamoon!</h1>
-      <p>Kirjaudu sisään tai rekisteröidy alla.</p>
+      <p>Kirjaudu sisään tai rekisteröidy alla</p>
       <div className="button-container">
         <Link to="/login" className="button">
           Kirjaudu
